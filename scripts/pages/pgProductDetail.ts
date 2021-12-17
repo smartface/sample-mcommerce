b4f9324e423710c205a6afdc9dd7af4250ce6976
fuatguzel <fuatguzel754@gmail.com>
2021-12-17 16:42:09 +0300

    add product detail, add some style changes.

--- scripts/pages/pgProductDetail.ts
+++ scripts/pages/pgProductDetail.ts
@@ -1,36 +1,33 @@
-import PgNumberDesign from 'generated/pages/pgNumber';
-import View from '@smartface/native/ui/view';
-import KeyboardType from '@smartface/native/ui/keyboardtype';
 import Application from '@smartface/native/application';
 import Color from '@smartface/native/ui/color';
 import HeaderBarItem from '@smartface/native/ui/headerbaritem';
+import PgProductDetailDesign from 'generated/pages/pgProductDetail';
 
-export default class PgNumber extends PgNumberDesign {
-    router: any;
+export default class PgProductDetail extends PgProductDetailDesign {
+    router: any
     leftItem: HeaderBarItem
+    rightItem: HeaderBarItem
 	constructor() {
 		super();
 		// Overrides super.onShow method
 		this.onShow = onShow.bind(this, this.onShow.bind(this));
 		// Overrides super.onLoad method
 		this.onLoad = onLoad.bind(this, this.onLoad.bind(this));
-
-        // this.imgBack.on(View.Events.Touch, () => {
-        //     this.router.goBack();
-        // })
-
-        this.btnRoute.on(View.Events.Touch, () => {
-            this.router.push('/pages/pgVerification')
-        })
-        
-        //this.mtbNumber.keyboardType = KeyboardType.NUMBER
+        this.headerBar.title = ""
 	}
-    addHeaderWithDirectImage() {
+    addLeftItem() {
         this.leftItem = new HeaderBarItem();
         this.leftItem.image = "images://backbtn.png";
         this.leftItem.color = Color.BLACK;
         this.headerBar.setLeftItem(this.leftItem);
     }
+    addRightItem() {
+        this.rightItem = new HeaderBarItem();
+        this.rightItem.image = "images://share.png";
+        this.rightItem.color = Color.BLACK;
+        this.headerBar.setItems([this.rightItem]);
+    }
+
 }
 
 /**
@@ -39,12 +36,10 @@ export default class PgNumber extends PgNumberDesign {
  * @param {function} superOnShow super onShow function
  * @param {Object} parameters passed from Router.go function
  */
-function onShow(this: PgNumber, superOnShow: () => void) {
+function onShow(this: PgProductDetail, superOnShow: () => void) {
 	superOnShow();
     Application.statusBar.visible = true;
-    //Application.statusBar.backgroundColor = Color.create("#000fff");
-    //this.headerbaritem.title = "Naber"
-    //this.headerBar.setLeftItem(this.headerbaritem)
+    Application.statusBar.backgroundColor = Color.create("#F2F3F2")
 }
 
 /**
@@ -52,7 +47,8 @@ function onShow(this: PgNumber, superOnShow: () => void) {
  * This event is called once when page is created.
  * @param {function} superOnLoad super onLoad function
  */
-function onLoad(this: PgNumber, superOnLoad: () => void) {
+function onLoad(this: PgProductDetail, superOnLoad: () => void) {
 	superOnLoad();
-    this.addHeaderWithDirectImage()
+    this.addLeftItem()
+    this.addRightItem()
 }
