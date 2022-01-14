82c90d7b618db7db3956c96640d143ef7029de86
fuatguzel <fuatguzel754@gmail.com>
2022-01-14 18:26:50 +0300

    fix: pgWelcome styles.

--- scripts/pages/pgWelcome.ts
+++ scripts/pages/pgWelcome.ts
@@ -5,41 +5,24 @@ import Application from '@smartface/native/application';
 import Color from '@smartface/native/ui/color';
 
 export default class PgWelcome extends PgWelcomeDesign {
-    router: any
-	constructor() {
-		super();
-		// Overrides super.onShow method
-		this.onShow = onShow.bind(this, this.onShow.bind(this));
-		// Overrides super.onLoad method
-		this.onLoad = onLoad.bind(this, this.onLoad.bind(this));
+    router: any;
+    constructor() {
+        super();
+        this.onShow = onShow.bind(this, this.onShow.bind(this));
+        this.onLoad = onLoad.bind(this, this.onLoad.bind(this));
         this.btnStart.on(View.Events.Touch, () => {
-            this.router.push('pgNumber')
-        })
-        this.lblWelcome.text = global.lang.welcomeText
-        this.lblSubtext.text = global.lang.welcomeSubText
-        this.btnStart.text = global.lang.getStarted
-	}
-}
-
-/**
- * @event onShow
- * This event is called when a page appears on the screen (everytime).
- * @param {function} superOnShow super onShow function
- * @param {Object} parameters passed from Router.go function
- */
-function onShow(this: PgWelcome, superOnShow: () => void) {
-	superOnShow();
-    if (System.OS !== 'iOS') {
-        Application.statusBar.visible = true;
-        Application.statusBar.backgroundColor = Color.WHITE
+            this.router.push('pgNumber');
+        });
+        this.lblWelcome.text = global.lang.welcomeText;
+        this.lblSubtext.text = global.lang.welcomeSubText;
+        this.btnStart.text = global.lang.getStarted;
     }
 }
 
-/**
- * @event onLoad
- * This event is called once when page is created.
- * @param {function} superOnLoad super onLoad function
- */
+function onShow(this: PgWelcome, superOnShow: () => void) {
+    superOnShow();
+}
+
 function onLoad(this: PgWelcome, superOnLoad: () => void) {
-	superOnLoad();
+    superOnLoad();
 }
