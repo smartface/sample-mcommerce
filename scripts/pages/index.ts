4891c002f6845882dff80231770ed765d7bd441f
alimoncul <alimoncul@gmail.com>
2022-01-14 10:28:30 +0300

    chore: remove selectLocation

--- scripts/pages/index.ts
+++ scripts/pages/index.ts
@@ -3,7 +3,6 @@ export { default as pgWelcome } from './pgWelcome';
 export { default as pgSignIn } from './pgSignIn';
 export { default as pgNumber } from './pgNumber';
 export { default as pgVerification } from './pgVerification';
-export { default as pgSelectLocation } from './pgSelectLocation';
 export { default as pgLogin } from './pgLogin';
 export { default as pgSignUp } from './pgSignUp';
 export { default as pgCategories } from './pgCategories';
