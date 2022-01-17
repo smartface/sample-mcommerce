fa1fecb4cd57f0d35f70913c633819a1cf333df4
fuatguzel <fuatguzel754@gmail.com>
2022-01-17 13:20:22 +0300

    Update style classes and remove comments.

--- scripts/pages/pgNumber.ts
+++ scripts/pages/pgNumber.ts
@@ -1,62 +1,49 @@
 import PgNumberDesign from 'generated/pages/pgNumber';
 import View from '@smartface/native/ui/view';
 import KeyboardType from '@smartface/native/ui/keyboardtype';
-import Application from '@smartface/native/application';
-import Color from '@smartface/native/ui/color';
 import HeaderBarItem from '@smartface/native/ui/headerbaritem';
-import System from '@smartface/native/device/system';
+import { NativeStackRouter } from '@smartface/router';
+import { getCombinedStyle } from '@smartface/extension-utils/lib/getCombinedStyle';
+import Image from '@smartface/native/ui/image';
+const { image } = getCombinedStyle('.sf-headerBar.close');
 
 export default class PgNumber extends PgNumberDesign {
-    router: any;
-    leftItem: HeaderBarItem
-	constructor() {
-		super();
-		// Overrides super.onShow method
-		this.onShow = onShow.bind(this, this.onShow.bind(this));
-		// Overrides super.onLoad method
-		this.onLoad = onLoad.bind(this, this.onLoad.bind(this));
+    router: NativeStackRouter;
+    leftItem: HeaderBarItem;
+    constructor() {
+        super();
+        this.onShow = onShow.bind(this, this.onShow.bind(this));
+        this.onLoad = onLoad.bind(this, this.onLoad.bind(this));
 
         this.btnRoute.on(View.Events.Touch, () => {
-            this.router.push('pgVerification')
-        })
-        this.lblTitle.text = global.lang.enterMobilNumberText
-        this.lblText.text = global.lang.mobileNumber
-	}
+            this.router.push('pgVerification');
+        });
+        this.lblTitle.text = global.lang.enterMobilNumberText;
+        this.lblText.text = global.lang.mobileNumber;
+    }
     initMaterialTextBox() {
         this.mtbNumber.options = {
-            text: "+90"
-        }
+            text: global.lang.phoneCode
+        };
         this.mtbNumber.materialTextBox.keyboardType = KeyboardType.NUMBER;
     }
     addHeaderWithDirectImage() {
-        this.leftItem = new HeaderBarItem();
-        this.leftItem.image = "images://backbtn.png";
-        this.leftItem.color = Color.BLACK;
+        this.leftItem = new HeaderBarItem({
+            image: Image.createFromFile(image),
+            onPress: () => {
+                this.router.goBack();
+            }
+        });
         this.headerBar.setLeftItem(this.leftItem);
     }
 }
 
-/**
- * @event onShow
- * This event is called when a page appears on the screen (everytime).
- * @param {function} superOnShow super onShow function
- * @param {Object} parameters passed from Router.go function
- */
 function onShow(this: PgNumber, superOnShow: () => void) {
-	superOnShow();
-    if (System.OS !== 'iOS') {
-        Application.statusBar.visible = true;
-        Application.statusBar.backgroundColor = Color.WHITE
-    }
+    superOnShow();
 }
 
-/**
- * @event onLoad
- * This event is called once when page is created.
- * @param {function} superOnLoad super onLoad function
- */
 function onLoad(this: PgNumber, superOnLoad: () => void) {
-	superOnLoad();
-    this.initMaterialTextBox()
-    this.addHeaderWithDirectImage()
+    superOnLoad();
+    this.initMaterialTextBox();
+    this.addHeaderWithDirectImage();
 }
