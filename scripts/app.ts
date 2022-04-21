c6d1f0e753fefe559fdbb4e17ce4584b9792cad5
Furkan Arabacı <furkan.arabaci@smartface.io>
2022-04-21 21:41:10 +0300

    update versions and add stack check

--- scripts/app.ts
+++ scripts/app.ts
@@ -11,12 +11,16 @@ import System from '@smartface/native/device/system';
 // Set uncaught exception handler, all exceptions that are not caught will
 // trigger onUnhandledError callback.
 Application.on("unhandledError", (e: UnhandledError) => {
-    const error = errorStackBySourceMap(e);
-    const message = {
-      message: System.OS === System.OSType.ANDROID ? error.stack : e.message,
-      stack: System.OS === System.OSType.IOS ? error.stack : undefined
-    };
+  const error = errorStackBySourceMap(e);
+  const message = {
+    message: System.OS === System.OSType.ANDROID ? error.stack : e.message,
+    stack: System.OS === System.OSType.IOS ? error.stack : undefined,
+  };
+  if(message.stack || System.OS === System.OSType.ANDROID){
     console.error("Unhandled Error: ", message);
     alert(JSON.stringify(message, null, 2), e.type || lang.applicationError);
-  });
+  }
+});
 router.push('/launchScreen/main');
+
+
