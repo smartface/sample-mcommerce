25ffeb7a495bd0f1a556e630db7e76a39fd9820a
erenkan <erenkn@gmail.com>
2022-05-17 18:47:46 +0300

    launch screen aspectfit

--- scripts/pages/pgLaunchScreen.ts
+++ scripts/pages/pgLaunchScreen.ts
@@ -1,11 +1,11 @@
 import PgLaunchScreenDesign from 'generated/pages/pgLaunchScreen';
 import { withDismissAndBackButton } from '@smartface/mixins';
 import { Router, Route } from '@smartface/router';
-import Dialog from "@smartface/native/ui/dialog";
+import Dialog from '@smartface/native/ui/dialog';
 import FlexLayout from '@smartface/native/ui/flexlayout';
 import Color from '@smartface/native/ui/color';
-import GifImageView from "@smartface/native/ui/gifimageview";
-import GifImage from "@smartface/native/ui/gifimage";
+import GifImageView from '@smartface/native/ui/gifimageview';
+import GifImage from '@smartface/native/ui/gifimage';
 import { IGifImage } from '@smartface/native/ui/gifimage/gifimage';
 import { ImageFillType } from '@smartface/native/ui/imageview/imageview';
 
@@ -22,19 +22,18 @@ export default class PgLaunchScreen extends withDismissAndBackButton(PgLaunchScr
         this.launchDialog = new Dialog({
             android: {
                 themeStyle: Dialog.Android.Style.ThemeNoHeaderBar
-            },
+            }
         });
         this.launchDialog.layout.alignItems = FlexLayout.AlignItems.CENTER;
         this.launchDialog.layout.justifyContent = FlexLayout.JustifyContent.CENTER;
         this.launchDialog.layout.backgroundColor = Color.create('#53b175');
-        this.gifImage = GifImage.createFromFile("assets://splash.gif");
+        this.gifImage = GifImage.createFromFile('assets://splash.gif');
         this.gifImageView = new GifImageView({
             //@ts-ignore
-            gifImage : this.gifImage,
+            gifImage: this.gifImage,
             height: 400,
-            width: 400,
-            imageFillType: ImageFillType.ASPECTFIT
-        })
+            width: 400
+        });
         this.launchDialog.layout.addChild(this.gifImageView);
         this.launchDialog.layout.applyLayout();
     }
@@ -42,11 +41,10 @@ export default class PgLaunchScreen extends withDismissAndBackButton(PgLaunchScr
         super.onShow?.();
         this.initBackButton(this.router);
         this.launchDialog.show();
-        setTimeout(() => { 
+        setTimeout(() => {
             this.launchDialog.hide();
-            this.router.push("/btb/tab1/home");
-        }, 2000)
-
+            this.router.push('/btb/tab1/home');
+        }, 2000);
     }
 
     public onLoad() {
